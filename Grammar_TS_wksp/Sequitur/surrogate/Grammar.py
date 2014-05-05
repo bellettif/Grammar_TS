529695c7f88b39104d768dc65d38528c52762436
Francois Belletti <francois.belletti13@imperial.ac.uk>
2014-05-05 22:37:18 +0100

    Tested sequitur on surrogate

---
+++ Grammar_TS_wksp/Sequitur/surrogate/Grammar.py
@@ -0,0 +1,62 @@
+'''
+Created on 5 mai 2014
+
+@author: francois
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from Rule import *
+
+class Grammar:
+    
+    def __init__(self, rule_dict):
+        self.rule_dict = rule_dict
+        for rule in self.rule_dict.values():
+            rule.compute_barcode(self.rule_dict)
+        all_symbols = rule_dict.keys()
+        for rule in rule_dict.values():
+            all_symbols.extend(rule.rhs)
+        all_symbols = list(set(all_symbols))
+        self.terminals = sorted(filter(lambda x : x not in rule_dict,
+                                all_symbols))
+        self.non_terminals = sorted(filter(lambda x : x in rule_dict,
+                                    all_symbols))
+        self.all_symbols = sorted(list(set.union(
+                              set(self.terminals),
+                              set(self.non_terminals))))
+        
+    def rand_seq(self, n_symbols, freqs):
+        freqs = np.asarray(freqs)
+        freqs /= np.sum(freqs)
+        print freqs
+        print self.all_symbols
+        print np.random.choice(self.all_symbols, freqs)
+        reduced_form = [np.random.choice(self.all_symbols, p = freqs)
+                        for i in xrange(n_symbols)]
+        freqs = zip(self.all_symbols, freqs)
+        expanded_form = []
+        for x in reduced_form:
+            print x
+            if x not in self.rule_dict:
+                expanded_form.append(x)
+            else:
+                expanded_form.extend(self.rule_dict[x].barcode)
+        return freqs, reduced_form, expanded_form
+        
+    def rand_seq_non_term(self, n_symbols, freqs):
+        freqs = np.asarray(freqs)
+        freqs /= np.sum(freqs)
+        print freqs
+        reduced_form = [np.random.choice(self.non_terminals, p = freqs)
+                        for i in xrange(n_symbols)]
+        freqs = zip(self.non_terminals, freqs)
+        expanded_form = []
+        for x in reduced_form:
+            if x not in self.rule_dict:
+                expanded_form.append(x)
+            else:
+                expanded_form.extend(self.rule_dict[x].barcode)
+        return freqs, reduced_form, expanded_form
+        
---
+++ Grammar_TS_wksp/Sequitur/surrogate/k_seq_on_surrogate.py
@@ -0,0 +1,45 @@
+'''
+Created on 5 mai 2014
+
+@author: francois
+'''
+
+import numpy as np
+
+from Rule import *
+from Grammar import *
+from k_compression.k_sequitur import k_seq_compress
+from k_seq_grammar_analysis import *
+
+grammar = {'-2' : Rule('-2', ['-1', '3']),
+           '-1' : Rule('-1', ['4', '5']),
+           '-5' : Rule('-5', ['1', '3']),
+           '-3' : Rule('-3', ['-1', '-5']),
+           '-6' : gen_framing_rule('-3', '6', '7', 3, 2),
+           '-4' : gen_power_rule('-4', '8', '9', 3)}
+
+my_grammar = Grammar(grammar)
+
+non_terminal_weights = np.random.power(5, len(my_grammar.non_terminals))
+terminal_weights = np.random.power(5, len(my_grammar.terminals))
+
+all_weights = list(non_terminal_weights)
+all_weights.extend(list(terminal_weights))
+
+freqs, reduced_form, expended_form =  my_grammar.rand_seq(1000, all_weights)
+
+print freqs
+print reduced_form
+print ''.join(expended_form)
+
+expended_form = map(int, expended_form)
+
+expended_form = np.asarray(expended_form, dtype = np.int32)
+
+print expended_form
+
+inferred_grammar = k_seq_compress(expended_form, 20, 20)
+
+print inferred_grammar
+
+plot_rules(inferred_grammar, 'Inferred', 'Example.png')
