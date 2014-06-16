1021e79fb59804ef6e3840601920f3593f38ce86
Francois Belletti <francois.belletti13@imperial.ac.uk>
2014-06-16 17:56:19 +0100

    Benchmark proba sequitur done

--- Grammar_TS_wksp/Inside-Outside/benchmark_proba_sequitur/scripts/Seq_on_examples/Run_on_noisy_grammar.py
+++ Grammar_TS_wksp/Inside-Outside/benchmark_proba_sequitur/scripts/Seq_on_examples/Run_on_noisy_grammar.py
@@ -4,26 +4,35 @@ Created on 16 juin 2014
 @author: francois
 '''
 
+import numpy as np
+
 from proba_sequitur.Proba_sequitur import Proba_sequitur
 from Grammar_examples.Grammar_examples import noisy_grammar
 
 
-samples = noisy_grammar.produce_sentences(100)
+samples = noisy_grammar.produce_sentences(1000)
+#samples = filter(lambda x : len(x) > 600, samples)
+
+print len(samples)
+print np.mean([len(x) for x in samples])
+
 
 ps = Proba_sequitur(samples,
                     samples,
                     6,
                     30,
                     True,
-                    0.1,
-                    0.1,
+                    0.0,
+                    0.0,
                     0.05)
 
 ps.run()
 
-absolute_counts = ps.absolute_counts
+absolute_counts = ps.relative_counts
 absolute_counts_items = absolute_counts.items()
-absolute_counts_items.sort(key = (lambda x : -sum(x[1])))
+absolute_counts_items.sort(key = (lambda x : -sum(x[1].values())))
+
+print absolute_counts
 
 for x in absolute_counts_items:
-    print 'Lhs ' + x[0]
+    print 'Lhs %.2f %s' % (sum(x[1].values()), x[0])
