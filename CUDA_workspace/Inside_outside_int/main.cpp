e86bc4d17f65d8508f2b89f66293cfe3070ae8b8
Francois Belletti <francois.belletti13@imperial.ac.uk>
2014-07-04 19:27:35 +0200

    Starting cuda sto grammar

---
+++ CUDA_workspace/Inside_outside_int/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+
+#include "sto_grammar.h"
+#include "choice.h"
+
+int main(void){
+
+	srand(time(NULL));
+
+	/*
+	 * Grammar example Figure 4 of Lari, Young 1987
+	 */
+	Sto_grammar simple_grammar(5, 2);
+	/*
+	 * Non terminal symbols
+	 */
+	simple_grammar.set_A(0, 1, 3, 0.3);
+	simple_grammar.set_A(0, 2, 4, 0.3);
+	simple_grammar.set_A(0, 1, 1, 0.2);
+	simple_grammar.set_A(0, 2, 2, 0.2);
+	simple_grammar.set_A(3, 0, 1, 1.0);
+	simple_grammar.set_A(4, 0, 2, 1.0);
+	/*
+	 * Terminal symbols
+	 */
+	simple_grammar.set_B(1, 0, 1.0);
+	simple_grammar.set_B(2, 1, 1.0);
+
+	/*
+	 * Grammar example Figure 7 of Lari, Young 1987
+	 */
+	Sto_grammar palindrom_grammar(7, 3);
+	/*
+	 * Non terminal symbols
+	 */
+	palindrom_grammar.set_A(0, 1, 2, 0.2);
+	palindrom_grammar.set_A(0, 3, 4, 0.2);
+	palindrom_grammar.set_A(0, 5, 6, 0.2);
+	palindrom_grammar.set_A(0, 5, 6, 0.11);
+	palindrom_grammar.set_A(0, 5, 6, 0.11);
+	palindrom_grammar.set_A(0, 5, 6, 0.11);
+
+
+	int n_samples = 10000;
+
+	int MAX_LENGTH = 256;
+
+	int * sentence = new int[MAX_LENGTH];
+	int length;
+
+	int error_code;
+
+	for(int i = 0; i < 100; ++i){
+		error_code = simple_grammar.produce_sentence(sentence,
+				length,
+				MAX_LENGTH);
+		if(error_code == 0){
+			for(int i = 0; i < length; ++i){
+				std::cout << sentence[i] << " ";
+			}std::cout << std::endl;
+		}else if(error_code == 1){
+			std::cout << "ERROR (TOO LONG)" << std::endl;
+		}else if(error_code == 2){
+			std::cout << "ERROR (LOOPING)" << std::endl;
+		}
+	}
+
+	delete [] sentence;
+
+	return 0;
+}
